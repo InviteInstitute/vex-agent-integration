4f47d983d2a27071a71c10bdd24a7da1898702d4
John Terracina <john@learndialogue.org>
2026-05-05 18:03:10 -0400

    production changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: [
+      'localhost', 'ec2-98-82-30-138.compute-1.amazonaws.com'
+    ]
   },
 });
